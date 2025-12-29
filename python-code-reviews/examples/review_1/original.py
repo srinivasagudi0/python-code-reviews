0e2dfea68ebe58c2b3a6c2a944c0ee8e638f2743
Srinivasa Gudi <239487102+srinivasagudi0@users.noreply.github.com>
2025-12-29 17:38:56 -0600

    Add files via upload

---
+++ python-code-reviews/examples/review_1/original.py
@@ -0,0 +1,6 @@
+# Original version (works but needs review)
+
+def calc(a, b):
+    return a*b/2
+
+print(calc(10, 5))
---
+++ python-code-reviews/examples/review_1/reviewed.py
@@ -0,0 +1,7 @@
+# Reviewed and improved version
+
+def calculate_triangle_area(base, height):
+    """Return the area of a triangle."""
+    return (base * height) / 2
+
+print(calculate_triangle_area(10, 5))
